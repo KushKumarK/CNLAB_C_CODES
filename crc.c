a6652860c339226c3ee0ef12c29f11776109019b
Kush Kumar Kushwaha <91371411+KushKumarK@users.noreply.github.com>
2023-07-05 13:19:52 +0530

    Update crc.c

--- crc.c
+++ crc.c
@@ -1,75 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-// Function to perform CRC error detection
-int performCRCCheck(char *data, char *divisor)
-{
-    int dataLen = strlen(data);
-    int divisorLen = strlen(divisor);
-    int i, j;
+#define N strlen(gen_poly)
 
-    // Append zeros to the data equal to the divisor length minus 1
-    for (i = 0; i < divisorLen - 1; i++)
-    {
-        data[dataLen + i] = '0';
-    }
-    data[dataLen + i] = '\0';
+char data[28];
+char check_value[28];
+char gen_poly[10];
+int data_length,i,j;
 
-    // Perform CRC division
-    for (i = 0; i < dataLen; i++)
-    {
-        if (data[i] == '1')
-        {
-            for (j = 0; j < divisorLen; j++)
-            {
-                if (data[i + j] == divisor[j])
-                {
-                    data[i + j] = '0';
-                }
-                else
-                {
-                    data[i + j] = '1';
-                }
-            }
-        }
+void XOR(){
+    for(j = 1;j < N; j++)
+    check_value[j] = (( check_value[j] == gen_poly[j])?'0':'1');
+}
 
-        // Print intermediate steps
-        printf("Iteration %d: %s\n", i + 1, data);
-    }
+void receiver(){
+    printf("Enter the received data: ");
+    scanf("%s", data);
+    printf("\n-----------------------------\n");
+    printf("Data received: %s", data);
+    crc();
+    for(i=0;(i<N-1) && (check_value[i]!='1');i++);
+        if(i<N-1)
+            printf("\nError detected\n\n");
+        else
+            printf("\nNo error detected\n\n");
+}
 
-    // Check if remainder is zero or not
-    for (i = 0; i < dataLen; i++)
-    {
-        if (data[i] == '1')
-        {
-            return 0; // Error detected
-        }
-    }
-    return 1; // No error detected
+void crc(){
+    for(i=0;i<N;i++)
+        check_value[i]=data[i];
+    do{
+        if(check_value[0]=='1')
+            XOR();
+        for(j=0;j<N-1;j++)
+            check_value[j]=check_value[j+1];
+        check_value[j]=data[i++];
+    }while(i<=data_length+N-1);
 }
 
 int main()
 {
-    char data[100];
-    char divisor[100];
-
-    printf("Enter the data bits: ");
-    scanf("%s", data);
-
-    printf("Enter the divisor: ");
-    scanf("%s", divisor);
-
-    printf("\nStep-by-Step Calculation:\n");
-
-    if (performCRCCheck(data, divisor))
-    {
-        printf("\nNo error detected. Data is correct.\n");
-    }
-    else
-    {
-        printf("\nError detected. Data is corrupted.\n");
-    }
-
-    return 0;
+    printf("\nEnter data to be transmitted: ");
+    scanf("%s",data);
+    printf("\n Enter the Generating polynomial: ");
+    scanf("%s",gen_poly);
+    data_length=strlen(data);
+    for(i=data_length;i<data_length+N-1;i++)
+        data[i]='0';
+    printf("\n----------------------------------------");
+    printf("\n Data padded with n-1 zeros : %s",data);
+    printf("\n----------------------------------------");
+    crc();
+    printf("\nCRC or Check value is : %s",check_value);
+    for(i=data_length;i<data_length+N-1;i++)
+        data[i]=check_value[i-data_length];
+    printf("\n----------------------------------------");
+    printf("\n Final data to be sent : %s",data);
+    printf("\n----------------------------------------\n");
+    receiver();
+        return 0;
 }
